06389f77466a05218431f86ddf907bf7db21c3f1
scarsty <scarsty@gmail.com>
2016-03-01 00:01:58 +0800

    no message

--- begin.py
+++ begin.py
@@ -1,18 +1,20 @@
 # ------------------------------------ 
 # 
-#
+# xie dian sha ma ??????
+# 
 # ------------------------------------
 
 import urllib2
+import urllib
 import re
 from bs4 import BeautifulSoup
 import HTMLParser
 
 # remove the html tags from the content
 def filter_tags(text):
-    text = re.sub(r'</?\w+[^>]*>','',text)
-    text = re.sub(r'<\![^>]*>','',text)
-    text = re.sub(r'[^\x00-\x7f]','',text)
+    text = re.sub(r'</?\w+[^>]*>', '', text)
+    text = re.sub(r'<\![^>]*>', '', text)
+    text = re.sub(r'[^\x00-\x7f]', '', text)
     return text
 
 # find the values in the content
@@ -23,7 +25,7 @@ def find_value(text, key):
     for k in range(len(words)):
         if words[k].lower()=='dielectric' and words[k+1].lower()=='constant':
             words_sub=words[k:k+10]
-            print words_sub
+            #print words_sub
             for word in words_sub:
                 if re.match('[0-9,.~]+',word):
                     word = re.sub(r'[,;~]','',word)
@@ -36,10 +38,17 @@ def find_value(text, key):
 
 # get text of one html page
 def get_text(url):
-    response = urllib2.urlopen(url)
-    content = response.read()
-    bs = BeautifulSoup(content, "html.parser")
-    text = bs.get_text()
+    if url[len(url)-4:len(url)]=='.pdf':
+        name = re.sub(r'\/', '', url)
+        name = re.sub(r'http:', '', name)
+        name = re.sub(r'https:', '', name)
+        urllib.urlretrieve(url, name) 
+        text = pdf2txt.pdf2txt(name)
+    else:
+        response = urllib2.urlopen(url)
+        content = response.read()
+        bs = BeautifulSoup(content, "html.parser")
+        text = bs.get_text()
     text = filter_tags(text)
     return text
 
@@ -66,27 +75,27 @@ def get_url_list(root_url):
 
 #------------------------------------------ Main Program ---------------------------------------------------
 
+if __name__ == '__main__':
+    # use bing to search the content
+    root_url = 'http://www.bing.com/search?q=barium+titanate+dielectric+constant'
 
-# use bing to search the content
-root_url = 'http://www.bing.com/search?q=barium+titanate+dielectric+constant'
+    url_list = get_url_list(root_url)
 
-#url_list = get_url_list(root_url)
+    # test code
+    #url = 'http://onlinelibrary.wiley.com/doi/10.1111/j.1551-2916.2008.02693.x/abstract'
+    #del alist[0]
+    #text = get_text(url)
+    #print text
+    #print find_value(text,['dielectric constant']), url
+    # -------------------------
 
-url = 'http://onlinelibrary.wiley.com/doi/10.1111/j.1551-2916.2008.02693.x/abstract'
-#del alist[0]
-text = get_text(url)
-#print text
-print find_value(text,['dielectric constant']), url
-
-
-for url in url_list:
-    break
-    try:
-        text = get_text(url)
-        print find_value(text,['dielectric constant']), url
-        #break
-    except:
-        print 111
+    for url in url_list:
+        #print url[len(url)-4:len(url)]
+        #try:
+            text = get_text(url)
+            print find_value(text,['dielectric constant']), url
+        #except:
+            print 'something wrong!', url
         
 
 
