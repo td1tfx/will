ecc847faf0a0fbcc85e838b7b3d349a0d507c7a4
scarsty <scarsty@gmail.com>
2016-04-19 18:40:08 +0800

    建立 vs python 工程

--- crawl/html2txt_test.py
+++ crawl/html2txt_test.py
@@ -1,4 +1,6 @@
 import re
+import pdf2txt
+import urllib
 
 
 def filter_tags(htmlstr):
@@ -41,3 +43,30 @@ def replaceCharEntity(htmlstr):
 
 def repalce(s,re_exp,repl_string):
     return re_exp.sub(repl_string,s)
+    
+
+
+# find the values in the content
+def find_value(text, key):
+    value=[]
+    keys = key[0].split()
+    words = text.split()
+    for k in range(len(words)):
+        if words[k].lower()=='dielectric' and words[k+1].lower()=='constant':
+            words_sub=words[k:k+10]
+            print words_sub
+            for word in words_sub:
+                if re.match('[0-9,.~]+',word):
+                    word = re.sub(r'[,;~]','',word)
+                    try:
+                        one_value = float(word)
+                        value.append(one_value)
+                    except:
+                        one_value = 0
+    return value
+    
+if __name__ == '__main__':
+    #url='http://www.iiss.sanu.ac.rs/download/vol40_3/vol40_3_02.pdf'
+    text = pdf2txt.pdf2txt('pdf/asianjournalofchemistry21102009.weebly.comuploads29712971446026-s117-s124.pdf')
+    print text
+    print find_value(text,['dielectric constant'])
