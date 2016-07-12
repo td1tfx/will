135eb2127b6836824311842d30b44c6f0d3cdd66
scarsty <scarsty@gmail.com>
2016-07-12 12:55:45 +0800

    option, use const std::string&

--- neural/src/Option.h
+++ neural/src/Option.h
@@ -5,33 +5,42 @@
 struct Option
 {
 	Option() {}
-	Option(const char* filename) { loadIni(filename); }
+	Option(const std::string& filename) { loadIni(filename); }
 	~Option() { delete _ini; }
 
 private:
 	INIReader* _ini;
 public:
-	void loadIni(const char* filename)
+	void loadIni(const std::string& filename)
 	{
 		_ini = new INIReader(filename);
 	}
 
-	int getInt(const char* name, int v = 0)
+	int getInt(const std::string& name, int v = 0)
 	{
 		return int(_ini->GetReal("will", name, v));
 	}
 	//注意这里只获取双精度数，如果是单精度模式会包含隐式转换
-	double getReal(const char* name, double v = 0.0)
+	double getReal(const std::string& name, double v = 0.0)
 	{
 		return _ini->GetReal("will", name, v);
-	}
-	/*float getReal(const char* name, float v = 0.0f)
-	{
-		return _ini->GetReal("will", name, v);
-	}*/
-	std::string getString(const char* name, std::string v = "")
+	}	
+	std::string getString(const std::string& name, std::string v = "")
 	{
 		return _ini->Get("will", name, v);
+	}
+
+	int getInt(const std::string& section, const std::string& name, int v = 0)
+	{
+		return int(_ini->GetReal(section, name, v));
+	}
+	double getReal(const std::string& section, const std::string& name, double v = 0.0)
+	{
+		return _ini->GetReal(section, name, v);
+	}
+	std::string getString(const std::string& section, const std::string& name, std::string v = "")
+	{
+		return _ini->Get(section, name, v);
 	}
 
 };
